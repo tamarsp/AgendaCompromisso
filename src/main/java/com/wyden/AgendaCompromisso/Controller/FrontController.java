6f1e4347ad2176727efc278bca426d75202ed746
Tamar <tamarpeixoto1@gmail.com>
2025-11-18 15:06:39 -0300

    parte19

--- src/main/java/com/wyden/AgendaCompromisso/Controller/FrontController.java
+++ src/main/java/com/wyden/AgendaCompromisso/Controller/FrontController.java
@@ -1,11 +1,10 @@
 package com.wyden.AgendaCompromisso.Controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PatchMapping;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class FrontController {
-	 @PatchMapping("/{path:[^\\.]+}/**")
+	 @RequestMapping("/{path:[^\\.]+}/**")
 	    public String forward() {
 	        return "forward:/index.html";
 	    }
