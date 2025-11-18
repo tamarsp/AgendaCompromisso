efb95bdb7d2f28d6135c042ffd4a830f94920a2f
Tamar <tamarpeixoto1@gmail.com>
2025-11-18 16:16:13 -0300

    parte21

--- frontAgenda/frontAgenda/vite.config.js
+++ frontAgenda/frontAgenda/vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',                    // ← OBRIGATÓRIO para rodar no Spring Boot!
+  base: './',                 
   plugins: [
     vue(),
     vueDevTools(),
