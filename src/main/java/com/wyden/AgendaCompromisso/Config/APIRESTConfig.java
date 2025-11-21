15e32f65e9aa932742f811975288f8b2ee9af060
Tamar <tamarpeixoto1@gmail.com>
2025-11-20 23:16:26 -0300

    Update APIRESTConfig.java

--- src/main/java/com/wyden/AgendaCompromisso/Config/APIRESTConfig.java
+++ src/main/java/com/wyden/AgendaCompromisso/Config/APIRESTConfig.java
@@ -14,7 +14,7 @@ public class APIRESTConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
 
-                registry.addMapping("/api/**") 
+                registry.addMapping("/**") 
                         .allowedOrigins(
                                 "http://localhost:5173",
                                 "http://127.0.0.1:5173",
