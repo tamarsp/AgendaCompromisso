416b8bc0617fd2ddd8793d924bc48e021aa712cd
Tamar <tamarpeixoto1@gmail.com>
2025-11-23 00:20:11 -0300

    44

--- frontAgenda/frontAgenda/vite.config.js
+++ frontAgenda/frontAgenda/vite.config.js
@@ -5,10 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // CORREÇÃO CRÍTICA: Define o caminho base como '/'
-  // Isso garante que todos os assets no index.html (como "assets/index-...") 
-  // sejam gerados com um caminho absoluto: "/assets/index-..."
-  
+   
   base: '/',
   
   plugins: [
